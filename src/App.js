80705479c30b9b1baa02421e999e4b173e519501
Ben Hardy <BHardy@intuition.com>
2019-10-14 12:02:41 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import Recipe from './Recipe'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+const App= ()=>{
+  const APP_ID = '9e72c845';
+  const APP_KEY = '1811cdf3d0e5e7ff25372be5a8a255c4'
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('') 
+  const [query, setQuery]=useState('Chicken')
+  
+  useEffect(()=>{
+    console.log('rendered')
+    getRecipes()
+  },[query])
+
+
+  
+  const getRecipes = async ()=>{
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json()
+    setRecipes(data.hits)
+    
+  }
+  
+const updateSearch = (e)=>{
+  setSearch(e.target.value)
 }
 
-export default App;
+const getSearch = (e)=>{
+  e.preventDefault()
+  setQuery(search)
+  setSearch('')
+}
+
+  return(
+    <div className="App">
+    <form onSubmit={getSearch}>
+      <input name="search" value={search}  onChange={updateSearch} />
+      <button  onClick={getSearch}>Search</button>
+      {recipes.map((info, i) =>(
+        <Recipe key={i} title={info.recipe.label} calories={parseInt(info.recipe.calories)} image={info.recipe.image} />
+      ))}
+    </form>
+    </div>
+  )
+}
+
+export default App
